c5e5e2e83494b1a986c1eaee01a75f8b52ec2b35
Ansh Talaviya <anshtalaviya22@gmail.com>
2024-10-05 05:05:10 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-primary text-light p-3 pl-5">
+            <h1 style={{marginLeft:"160px"}}>Apni Recipe</h1>
+        </header>
+    );
+};
+
+export default Header;
